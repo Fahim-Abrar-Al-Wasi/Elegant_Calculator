1c4568436e87a965b7db3984ef1a2ea72d4ca3c3
unknown <fahimabrarwasi@gmail.com>
2023-03-28 02:28:04 +0600

    Another java file added

---
+++ javaclass2.java
@@ -0,0 +1,16 @@
+public class javaclass2 {
+    private String name;
+    private int age;
+
+    public javaclass2(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
